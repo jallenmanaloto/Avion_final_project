bbd10671c1c7d1f62ff026e113c625bd0dce9d8c
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-11 20:50:29 +0800

    Created join table for Users and Establishments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_124434) do
+ActiveRecord::Schema.define(version: 2022_01_11_124928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 2022_01_11_124434) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "establishments_users", id: false, force: :cascade do |t|
+    t.bigint "establishment_id", null: false
+    t.bigint "user_id", null: false
   end
 
   create_table "requests", force: :cascade do |t|
