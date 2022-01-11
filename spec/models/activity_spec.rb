77da06065c877365045b0fa2fec96c146e954141
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-11 15:41:56 +0800

    Createc rspec test for Activity model

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:activity) { FactoryBot.build(:activity) }
+  
+  describe 'sending activity record' do
+    context 'with lacking parameters' do
+      it 'will return an error if name is not present' do
+        activity = Activity.new
+        activity.activity_type = 'Visit'
+
+        expect(activity).to_not be_valid
+        expect(activity.errors).to be_present
+      end
+
+      it 'will return an error if activity type is not present' do
+        activity = Activity.new
+        activity.name = 'Malls'
+
+        expect(activity).to_not be_valid
+        expect(activity.errors).to be_present
+      end
+    end
+
+    context 'with complete parameters' do
+      it 'will return a success record' do
+
+        expect(activity).to be_valid
+      end
+    end
+  end
 end
