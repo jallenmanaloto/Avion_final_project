8d572babfd3bb757862a99c5c4e403a481097b78
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-11 20:48:08 +0800

    Created Establishment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_120308) do
+ActiveRecord::Schema.define(version: 2022_01_11_124434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2022_01_11_120308) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "establishments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "requests", force: :cascade do |t|
