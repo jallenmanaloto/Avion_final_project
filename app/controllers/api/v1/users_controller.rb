1c6e951d338383fb523c34ba8f79ec10b5665557
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-11 16:15:24 +0800

    Created method to get list of all users and search specific users on request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,24 @@ module Api
 
             def index
                 users = User.all
+
+                render json: { users: users }
+            end
+
+            def search_user
+                user = User.where('LOWER(first_name) OR LOWER(last_name) LIKE ?', "%#{params[:search].downcase}%")
+                
+                render json: { user: user }
             end
 
             def show
+                user = User.find(params[:id])
+            end
+
+            private
+
+            def user_params
+                params.require(:user).permit(:first_name, :middle_name, :last_name, :role)
             end
         end
     end
