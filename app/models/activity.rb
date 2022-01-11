15a2d3df417e37348fb5bfd7c71d70bd3327bb88
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-11 19:57:12 +0800

    Add association to User

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,4 @@
 class Activity < ApplicationRecord
+    belongs_to :user
     validates :activity_type, :name, presence: true
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,4 @@
 class Request < ApplicationRecord
+    belongs_to :user
     validates :name, :request_type, presence: true
 end
