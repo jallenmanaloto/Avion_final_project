e6ef56fc4d4fb9e7c5a5fb4f1d85742d99c60c62
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-11 15:35:30 +0800

    Created spec test for request model

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Request, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:request) { FactoryBot.build(:request) }
+
+  describe 'requests sent' do
+    context 'with lacking parameters' do
+      it 'will throw an error if name is not present' do
+        request = Request.new
+        request.request_type = 'Certification'
+
+        expect(request).to_not be_valid
+        expect(request.errors).to be_present
+      end
+
+      it 'will throw an error if request type is not present' do
+        request = Request.new
+        request.name = 'Health Certification'
+
+        expect(request).to_not be_valid
+        expect(request.errors).to be_present
+      end
+    end
+
+    context 'with complete parameters' do
+      it 'will return a valid record' do
+
+        expect(request).to be_valid
+      end
+    end
+  end
 end
