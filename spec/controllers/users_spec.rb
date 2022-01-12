dee65a7e32f73469689963c66d80991631b42c7e
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-12 19:46:46 +0800

    Add rspec test on methods index and all_positive

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,13 +1,41 @@
 require 'rails_helper'
 
-describe 'Getting all Users with role as health_user', type: :request do
-    FactoryBot.create(:user)
-    
-    context 'accessing as an Admin' do
-        it 'gets list of users' do
+describe 'Users request', type: :request do
+    let!(:user) { FactoryBot.build(:user) }
 
-            get api_v1_users_path
-            expect(response).to have_http_status(200)
+    context 'Getting all list of users' do
+        before {get '/api/v1/users'}
+
+        it 'returns all users' do 
+            expect(JSON.parse(response.body).size).to_not be_nil
+        end
+
+        it 'returns a status code 200' do
+            expect(response).to have_http_status(:success)
+        end
+    end
+
+    context 'Getting list of User with covid_status of Positive' do
+        before {get '/api/v1/users/all_positive'}
+
+        it 'returns all users as a Hash' do 
+            expect(JSON.parse(response.body)).to be_an_instance_of(Hash)
+        end
+
+        it 'returns a status code 200' do
+            expect(response).to have_http_status(:success)
+        end
+    end
+
+    context 'Posting request to search for a user on search_user method' do
+        before {get '/api/v1/users/all_positive'}
+
+        it 'returns all users as a Hash' do 
+            expect(JSON.parse(response.body)).to be_an_instance_of(Hash)
+        end
+
+        it 'returns a status code 200' do
+            expect(response).to have_http_status(:success)
         end
     end
 end
