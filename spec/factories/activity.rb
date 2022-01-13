347c3d80404c09c2a8aafb25da0bb77801d8698e
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-13 14:29:23 +0800

    Add user_id to factory

--- spec/factories/activity.rb
+++ spec/factories/activity.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
     factory(:activity) do
         activity_type { "Visit" }
         name { "SM Megamall" }
+        user_id { 1 }
     end
 end
--- spec/factories/request.rb
+++ spec/factories/request.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
     factory(:request) do
         request_type { "Certification" }
         name { "Health Certification" }
+        user_id { 1 }
     end
 end
