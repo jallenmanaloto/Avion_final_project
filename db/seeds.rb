ee474a891363a37b0d7c0256724f191060367652
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-13 14:44:57 +0800

    Add default users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create({
+    last_name: 'Giovanna',
+    first_name: 'Giorno',
+    middle_name: 'jojo',
+    email: 'admin@project.com',
+    password: 'admin01',
+    password_confirmation: 'admin01',
+    role: 'admin'
+})
+
+health_users = User.create([
+    {
+    last_name: 'Kujo',
+    first_name: 'Jotaro',
+    middle_name: 'J',
+    email: 'Jotaro@kujo.com',
+    password: 'jotaropass',
+    password_confirmation: 'jotaropass',
+    covid_status: 'negative',
+    role: 'health_user'
+    },
+    {
+    last_name: 'Polnareff',
+    first_name: 'Jean Pierre',
+    middle_name: 'P',
+    email: 'JP@polnareff.com',
+    password: 'polnareffpass',
+    covid_status: 'positive',
+    role: 'health_user'
+    },
+    {
+    last_name: 'Speedwagon',
+    first_name: 'Robert',
+    middle_name: 'E.O',
+    email: 'reo@speedwagon.com',
+    password: 'speedwagonpass',
+    covid_status: 'negative',
+    role: 'health_user'
+    }
+])
