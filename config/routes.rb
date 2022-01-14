6350eac42b9e09b44253a0e30487f9d2ca2e80c7
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-14 15:19:57 +0800

    Add requests index method to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'api/v1/users', to: 'api/v1/users#index'
+  get 'api/v1/requests', to: 'api/v1/requests#index'
 
   namespace :api do
     namespace :v1 do
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
         get :index, on: :member
         get :get_users, on: :member
       end
+      resource :request
     end
   end
 end
