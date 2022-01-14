4f6205f741fd026a28c88d0757ebc5afdff2c1a9
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-14 15:21:00 +0800

    Add created method to create establishment record for a specific user

--- app/controllers/api/v1/establishments_controller.rb
+++ app/controllers/api/v1/establishments_controller.rb
@@ -8,11 +8,28 @@ module Api
                 render json: { establishments: establishments }
             end
 
+            def create
+                user = User.find(params[:id])
+                establishment = user.establishments.create(establishment_params)
+
+                if establishment.save
+                    render json: { establishment: establishment }
+                else
+                    render json: { error: establishment.errors }
+                end
+            end
+
             def get_users
                 establishment = Establishment.find(params[:id])
                 users = establishment.users
 
                 render json: { users: users }
+            end
+
+            private
+
+            def establishment_params
+                params.permit(:name)
             end
         end
     end
