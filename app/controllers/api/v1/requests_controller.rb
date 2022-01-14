2db4fa3936c991bc3d351305b3d53c4a70d2be37
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-14 15:20:28 +0800

    Add a create method to create requests for a specific user

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -1,11 +1,34 @@
 module Api
     module V1
         class RequestsController < ApplicationController
+            # before_action :get_user, only: [:create]
             
             def index
                 requests = Request.all
 
                 render json: { requests: requests }
+            end
+
+            def create
+                user = User.find(params[:user_id])
+                request = user.requests.build(request_params)
+                if request.save
+                    request.update(requested_by: "#{user.first_name} #{user.last_name}")
+                    render json: { request: request }
+                else
+                    render json: { error: request.errors }
+                end
+                
+            end
+
+            private
+
+            def get_user
+                user = User.find(params[:user_id])
+            end
+
+            def request_params
+                params.permit(:name, :request_type, :user_id)
             end
         end
     end
