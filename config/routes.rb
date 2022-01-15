d68359c0f40d39ac9e4b02926c813b5cc62e61e3
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-15 14:39:13 +0800

    Created route for create method on items controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :items, only: [:create]
       resource :users do
         get :all_positive, on: :member
         get :user_activities, on: :member
