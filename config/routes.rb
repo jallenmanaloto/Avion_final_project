7ef925b5528700404f047bc532ee86659d1bd8dc
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-15 15:49:02 +0800

    Add custom registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: 'users/registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'api/v1/users', to: 'api/v1/users#index'
   get 'api/v1/requests', to: 'api/v1/requests#index'
