7ada61f878229271cf1bcb09395f3fcd23944f2b
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-19 16:11:41 +0800

    Changed params permitted on registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,21 @@
 class Users::RegistrationsController < DeviseTokenAuth::RegistrationsController
-    respond_to :json
+    wrap_parameters false
 
     def create
-        user = User.new(user_params)
+        user = User.new(sign_up_params)
 
         if user.save
             user.update(:role => "health_user", :covid_status => "negative")
+            render json: { user: user }
+        else
+            render json: { message: user.errors }
         end
-        render json: { user: user }
+        
     end
 
     private
 
-    def user_params
+    def sign_up_params
         params.permit(:first_name, :middle_name, :last_name, :password, :email)
     end
 end
