bcfa5281538810707baf714024f4d9823c2f78bd
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-23 14:37:56 +0800

    Customized route for user_request method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'api/v1/users', to: 'api/v1/users#index'
   get 'api/v1/requests', to: 'api/v1/requests#index'
+  get 'api/v1/get_users/:id', to: 'api/v1/establishments#get_users'
 
   namespace :api do
     namespace :v1 do
@@ -11,12 +12,12 @@ Rails.application.routes.draw do
         get :all_positive, on: :member
         get :user_activities, on: :member
         get :user_establishments, on: :member
-        get :user_requests, on: :member
+        get 'user_requests/:id', to: 'users#user_requests'
         post :search_user, on: :member
       end
       resource :establishment do
         get :index, on: :member
-        get :get_users, on: :member
+        # get :get_users, on: :member
       end
       resource :request
     end
