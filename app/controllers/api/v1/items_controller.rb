715958b84d1faa77094104f049802bfc03b4dbdb
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-27 16:46:04 +0800

    Created show method to view specific uploaded item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,9 +2,17 @@ module Api
     module V1
         class ItemsController < ApplicationController
 
+            def show
+                user = User.find(params[:id])
+                item = user.items.last
+
+                render json: item
+            end
+
             def create
+                user = User.find(params[:id])
                 image = Cloudinary::Uploader.upload(params[:image])
-                item = Item.create(image: image["url"])
+                item = user.items.create(image: image["url"])
 
                 render json: item
             end
