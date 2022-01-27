d39067541cc81e6e7e5d75c2b59c3b6ec9ff2f26
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-27 21:47:10 +0800

    Add methods create and user activity

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -2,10 +2,29 @@ module Api
     module V1
         class ActivitiesController < ApplicationController
             
-            def index
-                activities = Activity.all
+            def create
+                user = User.find_by(uid: params[:uid])
+                activity = user.activities.create(activities_params)
 
-                render json: { requests: activities }
+                if activity.save
+                    activity.update(:userVisit => "#{user.first_name} #{user.last_name}", :userEmail => user.email, :userAddress => user.address, :userStatus => user.covid_status)
+                    render json: { activity: activity, user: user }
+                else
+                    render json: {error: activity.errors}
+                end
+            end
+
+            def user_activity
+                estab_param = params[:name]
+                activity = Activity.where(name: estab_param)
+                
+                render json: { users: activity}
+            end
+
+            private
+
+            def activities_params
+                params.permit(:name, :userAddress, :userEmail, :userVisit, :userStatus)
             end
         end
     end
