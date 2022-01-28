19f01f6999772cd882f6452c9b96ef3f33f386f4
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-28 13:00:13 +0800

    Created rspec test for Item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Invalid or empty image attribute" do
+    it 'will return an error' do
+      item = Item.new
+      item.image = nil
+
+      expect(item).to_not be_valid
+    end
+  end
+
+  context "Valid image attribute" do
+    it 'will return a success record' do
+
+      user = User.new
+      user.first_name = 'John'
+      user.last_name = 'Doe'
+      user.middle_name = 'M'
+      user.email = 'johndoevalid@email.com'
+      user.password = 'johnnypassword'
+
+      item = user.items.new
+      item.image = 'sampleurl'
+
+      expect(item).to be_valid
+    end
+  end
 end
