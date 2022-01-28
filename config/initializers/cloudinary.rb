411b0b1fe368b9f4b475f66ce7bd8ca665f6d1d6
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-28 14:59:24 +0800

    Secured sensitive key from cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|    
-    config.cloud_name = "dadaqbzuo"   
-    config.api_key = "896398739586594"   
-    config.api_secret = "ku2Lx-LyWwaXA9srVF11bHhrdcM"   
+    config.cloud_name = ENV['CLOUD_NAME']  
+    config.api_key = ENV['API_KEY']   
+    config.api_secret = ENV['API_SECRET']    
     config.secure = true    
     config.cdn_subdomain = true  
  end
