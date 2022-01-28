2582692aea0232a878232bf1978e158b7546bb56
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-28 16:42:30 +0800

    add method to skip confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+        after_create :skip_confirmation
         include DeviseTokenAuth::Concerns::User
                 # Include default devise modules.
                 devise :database_authenticatable, :registerable,
-                        :recoverable, :rememberable, :validatable, :confirmable
+                        :recoverable, :rememberable, :validatable
         # Include default devise modules. Others available are:
         # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
         validates :last_name, :first_name, :middle_name, :email, presence: true
@@ -13,4 +14,9 @@ class User < ApplicationRecord
         has_many :appointments
         has_many :items
         has_and_belongs_to_many :establishments
+
+        def skip_confirmation
+                self.skip_confirmation!
+                self.confirm!
+        end
 end
