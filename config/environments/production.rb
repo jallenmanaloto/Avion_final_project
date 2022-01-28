8046c84af2797848997b294b4e2bbfe23ba8aece
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-28 16:36:43 +0800

    Add action mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   Rails.application.routes.default_url_options[:host] = 'health-users-api.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => "https://health-users-api.herokuapp.com/" }
 end
